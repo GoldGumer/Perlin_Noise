078c6a4d697d3e7958f983fe3785c283da6d8343
up2059509 <up2059509@W0DEL-1-125D-11>
2023-01-27 16:36:46 +0000

    initial commit

---
+++ Assets/Perlin_Noise_Manager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perlin_Noise_Manager : MonoBehaviour
+{
+    [SerializeField] ComputeShader perlin_Noise;
+    public RenderTexture render_Texture = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (render_Texture == null)
+        {
+            render_Texture = new RenderTexture(source.width, source.height, 24);
+            render_Texture.enableRandomWrite = true;
+            render_Texture.Create();
+        }
+
+        perlin_Noise.SetTexture(0, "Result", render_Texture);
+        perlin_Noise.SetFloats("screen", new float[2] { render_Texture.width, render_Texture.height });
+        perlin_Noise.SetFloat("time", Time.deltaTime);
+
+        uint x, y, z;
+
+        perlin_Noise.GetKernelThreadGroupSizes(0, out x, out y, out z);
+        perlin_Noise.Dispatch(0, (int)(render_Texture.width / x), (int)(render_Texture.height / y), (int)z);
+
+        Graphics.Blit(render_Texture, destination);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
